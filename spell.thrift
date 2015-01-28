1ba6f94a8d1aea7e793a90ca51feaf638ad1950e
KGG814 <kalana.gamlath1@gmail.com>
2015-01-28 16:09:45 -0500

    First commit

---
+++ spell.thrift
@@ -0,0 +1,12 @@
+struct SpellRequest {
+  1: list<string> to_check
+}
+
+struct SpellResponse {
+  1: list<bool> is_correct
+}
+
+service SpellService {
+  SpellResponse spellcheck(1:SpellRequest request)
+}
+
